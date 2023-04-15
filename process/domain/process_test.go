41ccfdcb6833dce99891859cb5d2b8ae6d0cc6c1
Esteban Contreras <contacto@econtreras.net>
2023-04-15 12:30:27 -0400

    fix(test): removing unnecessary black line

--- process/domain/process_test.go
+++ process/domain/process_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestAddProcessWithoutOptions(t *testing.T) {
-
 	id, _ := uuid.NewUUID()
 	_, err := NewProcess(
 		id.String(),
@@ -22,7 +21,6 @@ func TestAddProcessWithoutOptions(t *testing.T) {
 }
 
 func TestAddProcessWithSalary(t *testing.T) {
-
 	id, _ := uuid.NewUUID()
 	_, err := NewProcess(
 		id.String(),
