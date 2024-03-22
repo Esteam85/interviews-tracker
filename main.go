b1737b5118847e2c1bc543d13c7e11def7036cbe
Esteban Contreras <contacto@econtreras.net>
2024-03-22 20:39:02 -0300

    feat: adding add process handler

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
-	
+import (
+	"github.com/esteam85/interviews-tracker/process/infrastructure"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.POST("/processes", infrastructure.AddProcessHandler)
+	err := router.Run("localhost:8080")
+	if err != nil {
+		panic("http server fail!")
+	}
 }
