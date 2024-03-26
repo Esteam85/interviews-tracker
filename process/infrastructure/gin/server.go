193252d4aaa952a52d89693e4de4b7def7236a86
Esteban Contreras <contacto@econtreras.net>
2024-03-26 20:54:46 -0300

    feat: adding authentication middleware

--- process/infrastructure/gin/server.go
+++ process/infrastructure/gin/server.go
@@ -1,7 +1,31 @@
 package gin
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/url"
+	"os"
+	"time"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/jwks"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
+	"github.com/gin-gonic/gin"
+	adapter "github.com/gwatts/gin-adapter"
+)
 
 func NewEngine() *gin.Engine {
-	return gin.Default()
+	r := gin.Default()
+	issuerURL, _ := url.Parse(os.Getenv("AUTH0_ISSUER_URL"))
+	audience := os.Getenv("AUTH0_AUDIENCE")
+
+	provider := jwks.NewCachingProvider(issuerURL, 5*time.Minute)
+
+	jwtValidator, _ := validator.New(provider.KeyFunc,
+		validator.RS256,
+		issuerURL.String(),
+		[]string{audience},
+	)
+
+	jwtMiddleware := jwtmiddleware.New(jwtValidator.ValidateToken)
+	r.Use(adapter.Wrap(jwtMiddleware.CheckJWT))
+	return r
 }
